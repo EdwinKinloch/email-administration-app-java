7be9cdac1547c7edfe54c7c7d01b0fb57c06ec1c
ekinloch <edwin.kinloch@infor.com>
2023-01-26 16:05:21 -0600

    Created a new method to set Department variable

--- emailapp/email.java
+++ emailapp/email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class email {
     private String firstName;
     private String lastName;
@@ -13,13 +15,29 @@ public class email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("Email created: " + this.firstName + " " + this.lastName);
+
+        //Call a method asking for the department and returning the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
 
 
     // Ask for the department
-
-
-    // Set the mailbox capacity
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
+        // Set the mailbox capacity
 
 
     // Set the alternate email
