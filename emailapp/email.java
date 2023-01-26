3bb0de5ae28c9f418acfb77ab5f266e56a68a966
ekinloch <edwin.kinloch@infor.com>
2023-01-26 14:57:32 -0600

    Added the variables and constructor for the email class.

---
+++ emailapp/email.java
@@ -0,0 +1,30 @@
+package emailapp;
+
+public class email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and last name
+    public email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created: " + this.firstName + " " + this.lastName);
+    }
+
+
+    // Ask for the department
+
+
+    // Set the mailbox capacity
+
+
+    // Set the alternate email
+
+
+    // Change the password
+}
+
---
+++ emailapp/emailapp.java
@@ -0,0 +1,9 @@
+package emailapp;
+
+
+public class emailapp {
+    public static void main(String[]args){
+        email em1 = new email("Edwin", "Kinloch");
+    }
+
+}
